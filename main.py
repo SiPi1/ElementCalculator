afde52dc60b7ce1f1b4d5ac182c8f6ea4c33d15a
SiPi1 <84973602+SiPi1@users.noreply.github.com>
2024-05-31 21:08:01 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
+import element.py
+
 def main():
     print("Hello? Would you like to make an element!")
     element = Element(input("How many protons?"), input("How many neutrons?"), input("How many electrons?"))
     print("Name: " + element.getName())
     print("Symbol: " + element.getSymbol())
-    print("Charge: " + element.getCharge())
-    print("Stable? " + element.isStable())
+    print("Charge: " + str(element.getCharge()))
+    print("Stable? Probably ")
+    if not(element.isStable()):
+        print("not")
+    print("Orbitals: " + element.getOrbitals())
 
 
 main()
