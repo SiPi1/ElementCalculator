fd7b13ac82ab45d11819db5c970f502a65151207
SiPi1 <84973602+SiPi1@users.noreply.github.com>
2024-05-31 21:08:34 -0400

    Update orbitals.py

--- orbitals.py
+++ orbitals.py
@@ -3,7 +3,7 @@ class orbital:
     def __init__(self, energyLevel):
         self.e = 0
         self.energyLevel = energyLevel
-        self.capacity = 2
+        self.capacity = 0
 
     def isFull(self):
         return self.e == self.capacity
@@ -15,9 +15,15 @@ class orbital:
             return 3
         else: 
             return 0
+
+    def getElectrons(self):
+        return self.e
+
+    def __str__(self):
+        return ""
     
     def add(self, electrons):
-        self.e += electrons #convert to int
+        self.e += int(electrons)
 
         if self.e > self.capacity:
             leftover = self.e - self.capacity
@@ -33,18 +39,35 @@ class orbitalS(orbital):
     def __init__(self, energyLevel):
         super().__init__(energyLevel)
         self.capacity = 2
-
+    
+    def __str__(self):
+        if self.e > 0:
+            return str(self.energyLevel) + "S^" + str(self.e) + ", "
+        return ""
 class orbitalP(orbital):
     def __init__(self, energyLevel):
         super().__init__(energyLevel)
         self.capacity = 6
-
+    
+    def __str__(self):
+        if self.e > 0:
+            return str(self.energyLevel) + "P^" + str(self.e) + ", "
+        return ""
 class orbitalD(orbital):
     def __init__(self, energyLevel):
         super().__init__(energyLevel)
         self.capacity = 10
-
+    
+    def __str__(self):
+        if self.e > 0:
+            return str(self.energyLevel) + "D^" + str(self.e) + ", "
+        return ""
 class orbitalF(orbital):
     def __init__(self, energyLevel):
         super().__init__(energyLevel)
         self.capacity = 12
+    
+    def __str__(self):
+        if self.e > 0:
+            return str(self.energyLevel) + "F^" + str(self.e) + ", "
+        return ""
