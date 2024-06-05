e6971bfb87eeaf8786ebe6c7b5dbea9c9ab3125d
SiPi1 <84973602+SiPi1@users.noreply.github.com>
2024-06-05 17:54:20 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,22 @@ import element.py
 
 def main():
     print("Hello? Would you like to make an element!")
-    element = Element(input("How many protons?"), input("How many neutrons?"), input("How many electrons?"))
+    element = Element(input("How many protons? "), input("How many neutrons? "), input("How many electrons? "))
+    
     print("Name: " + element.getName())
     print("Symbol: " + element.getSymbol())
     print("Charge: " + str(element.getCharge()))
-    print("Stable? Probably ")
-    if not(element.isStable()):
-        print("not")
+    print("Stable? " + ("Probably" if element.getStable() == 0 else "Unlikely"))
     print("Orbitals: " + element.getOrbitals())
-
+    
+    while (not(element.getStable() == 0) and input("Simulate decay? [Y/n] ") == "Y"):
+        print()
+        print("Protons: " + element.p)
+        print("Neutrons: " + element.n)
+        print("Name: " + element.getName())
+        print("Symbol: " + element.getSymbol())
+        print("Charge: " + str(element.getCharge()))
+        print("Stable? " + ("Probably" if element.getStable() == 0 else "Unlikely"))
+``
 
 main()
